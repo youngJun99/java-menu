d7916f5bf641ba15f318c8115e9782b22e964b82
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-11-24 16:04:03 +0900

    feat: 메뉴 Enum 구현

--- src/main/java/menu/constants/Errors.java
+++ src/main/java/menu/constants/Errors.java
@@ -3,7 +3,7 @@ package menu.constants;
 public enum Errors {
 
     //시스템 에러
-    SYSTEM_ERROR_UNKNOWN_MENU_INDEX("메뉴 인덱스가 존재하지 않습니다");
+    UNKNOWN_MENU_INDEX("메뉴 인덱스가 존재하지 않습니다");
 
 
     private static final String PREFIX = "[ERROR] ";
---
+++ src/main/java/menu/constants/MenuEnum.java
@@ -0,0 +1,22 @@
+package menu.constants;
+
+import java.util.List;
+
+public enum MenuEnum {
+
+    일식(1, "일식", "규동,우동,미소시루,스시,가츠동,오니기리,하이라이스,라멘,오코노미야끼"),
+    한식(2, "한식", "김밥,김치찌개,쌈밥,된장찌개,비빔밥,칼국수,불고기,떡볶이,제육볶음"),
+    중식(3, "중식", "깐풍기,볶음면,동파육,짜장면,짬뽕,마파두부,탕수육,토마토 달걀볶음,고추잡채"),
+    아시안(4, "아시안", "팟타이,카오 팟,나시고렝,파인애플 볶음밥,쌀국수,똠얌꿍,반미,월남쌈,분짜"),
+    양식(5, "양식", "라자냐,그라탱,뇨끼,끼슈,프렌치 토스트,바게트,스파게티,피자,파니니");
+
+    private final int categoryIndex;
+    private final String foodCategory;
+    private final List<String> menus;
+
+    MenuEnum(int categoryIndex, String foodCategory, String menus) {
+        this.categoryIndex = categoryIndex;
+        this.foodCategory = foodCategory;
+        this.menus = List.of(menus.split(","));
+    }
+}
