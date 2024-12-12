83ebfbaaf29f9c16ce090342004683b7c22381c5
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-12-13 01:43:17 +0900

    refactor: 시작 문구 추가

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -5,6 +5,8 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class InputView {
 
     public String getCoaches() {
+        System.out.println("점심 메뉴 추천을 시작합니다.");
+        System.out.println();
         System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
         return readLine();
     }
