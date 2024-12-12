7cd17e8d2fe3f03df7dc693f3a2115a58882d94b
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-12-12 23:22:09 +0900

    feat: MenuCategory 구현 완료 및 리드미 업데이트

---
+++ src/main/java/menu/MenuCategory.java
@@ -0,0 +1,24 @@
+package menu;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public enum MenuCategory {
+    일식(1, Arrays.asList("규동,우동,미소시루,스시,가츠동,오니기리,하이라이스,라멘,오코노미야끼".split(","))),
+    한식(2, Arrays.asList("김밥,김치찌개,쌈밥,된장찌개,비빔밥,칼국수,불고기,떡볶이,제육볶음".split(","))),
+    중식(3, Arrays.asList("깐풍기,볶음면,동파육,짜장면,짬뽕,마파두부,탕수육,토마토 달걀볶음,고추잡채".split(","))),
+    아시안(4, Arrays.asList("팟타이,카오 팟,나시고렝,파인애플 볶음밥,쌀국수,똠얌꿍,반미,월남쌈,분짜".split(","))),
+    양식(5, Arrays.asList("라자냐,그라탱,뇨끼,끼슈,프렌치 토스트,바게트,스파게티,피자,파니니".split(",")));
+
+    private final int index;
+    private final List<String> menus;
+
+    MenuCategory(int index, List<String> menus) {
+        this.index = index;
+        this.menus = menus;
+    }
+}
+
+
+
