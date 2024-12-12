afbf83d60bda507501ea16f1bce59e5d8206d382
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-12-13 00:25:54 +0900

    feat: 입력 형식 관련 에러 추가

--- src/main/java/menu/constants/Errors.java
+++ src/main/java/menu/constants/Errors.java
@@ -8,7 +8,9 @@ public enum Errors {
     //코치 관련 에러
     COACH_NAME_RANGE("코치의 이름은 %d 글자에서 %d 글자 사이로 입력해야 합니다."),
     COACHES_RANGE("코치들은 최소 %d명에서 %d명 사이로 입력해야 합니다."),
-    UNEATABLE_FOODS_RANGE("못먹는 음식은 최소 %d개이상 %d 이하여야 합니다.");
+    UNEATABLE_FOODS_RANGE("못먹는 음식은 최소 %d개이상 %d 이하여야 합니다."),
+    INVALID_COACH_INPUT("코치들을 올바르게 입력하지 않았습니다."),
+    INVALID_COACH_UNEATABLE_FOOD_INPUT("못 먹는 음식의 입력 형식이 올바르지 않습니다.");
 
 
     private static final String PREFIX = "[ERROR] ";
