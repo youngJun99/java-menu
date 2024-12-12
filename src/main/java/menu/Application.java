bddb18e801077aaf83831a394ae93c93c0c9895a
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-12-13 00:59:34 +0900

    feat: 중간 저장

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,14 @@
 package menu;
 
+import menu.domain.Coaches;
+import menu.handler.InputHandler;
+import menu.handler.InputValidator;
+import menu.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputHandler inputHandler = new InputHandler(new InputView(), new InputValidator());
+        Coaches coaches = inputHandler.getCoaches();
+        System.out.println(coaches);
     }
 }
