cd545d2f0cb9635eac631ec6fc40fe166521bedd
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-12-13 01:38:54 +0900

    feat: Controller 구현 완료

---
+++ src/main/java/menu/controller/Controller.java
@@ -0,0 +1,26 @@
+package menu.controller;
+
+import menu.dto.CoachMenuDto;
+import menu.dto.WeeklyMenuCategoryDto;
+import menu.service.RandomMenuService;
+import menu.view.OutputView;
+
+import java.util.List;
+
+public class Controller {
+
+    private final RandomMenuService randomMenuService;
+    private final OutputView outputView;
+
+    public Controller(RandomMenuService randomMenuService, OutputView outputView) {
+        this.randomMenuService = randomMenuService;
+        this.outputView = outputView;
+    }
+
+    public void run(){
+        randomMenuService.initializeService();
+        WeeklyMenuCategoryDto weeklyMenuCategoryDto = randomMenuService.getWeeklyMenuCategory();
+        List<CoachMenuDto> coachMenuDtos = randomMenuService.getCoachMenus();
+        outputView.printResults(weeklyMenuCategoryDto,coachMenuDtos);
+    }
+}
