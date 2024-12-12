e14b186d384240cb7d843d59d3187117ee778734
나용준 <141994188+youngJun99@users.noreply.github.com>
2024-12-13 01:13:57 +0900

    feat: 주간 카테고리를 아예 만들어서 주도록 RandomCategoryPicker에 매서드 구현

--- src/main/java/menu/domain/random/RandomCategoryPicker.java
+++ src/main/java/menu/domain/random/RandomCategoryPicker.java
@@ -3,13 +3,29 @@ package menu.domain.random;
 import camp.nextstep.edu.missionutils.Randoms;
 import menu.constants.MenuCategory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RandomCategoryPicker {
 
+    public static final int DAYS_TO_EAT = 5;
     private static final int CATEGORY_UPPER_LIMIT = 5;
     private static final int CATEGORY_LOWER_LIMIT = 1;
 
+    public List<MenuCategory> getWeeklyCategory() {
+        List<MenuCategory> menuCategories = new ArrayList<>();
+        while(menuCategories.size() < DAYS_TO_EAT){
+            MenuCategory randomCategory = pickRandomCategory();
+            if(menuCategories.stream()
+                    .filter(menuCategory -> menuCategory.equals(randomCategory))
+                    .count() <= 1) {
+                menuCategories.add(randomCategory);
+            }
+        }
+        return menuCategories;
+    }
 
-    public MenuCategory pickRandomCategory() {
+    private MenuCategory pickRandomCategory() {
         int randomIndex = Randoms.pickNumberInRange(CATEGORY_LOWER_LIMIT,CATEGORY_UPPER_LIMIT);
         return MenuCategory.ofIndex(randomIndex);
     }
